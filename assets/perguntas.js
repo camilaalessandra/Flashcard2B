65e0fec0c20d20096a65c3b65a656ce41986df1b
Camiis <160610734+camilaalessandra@users.noreply.github.com>
2024-10-15 09:17:29 -0300

    aula06

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'UO CSS é uma linguagem de estilização'
+)
